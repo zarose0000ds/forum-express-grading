d98dab2fb82c93860e4dc34924d27c7a2273e302
zarose0000ds <zarose0000ds@gmail.com>
2021-11-09 00:31:37 +0800

    feat: initially add user and admin home pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
-module.exports = (app) => {
+const adminController = require('../controllers/adminController')
+const restController = require('../controllers/restController')
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+module.exports = (app) => {
+  // ADMIN
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
+
+  // USER
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', restController.getRestaurants)
 }
