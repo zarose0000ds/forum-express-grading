03df9be49f10d4807c0cb0d1020f1601f12b44da
zarose0000ds <zarose0000ds@gmail.com>
2021-11-08 18:47:22 +0800

    feat: initialize project

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// TEMPLATE ENGINE
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+// ROUTES
 require('./routes')(app)
 
+// LISTENING
+app.listen(port, () => {
+  console.log(`The app is listening at http://localhost:${port}`)
+})
+
+// FOR TEST AUTOMATION
 module.exports = app
