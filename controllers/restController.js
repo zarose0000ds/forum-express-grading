ad34b3b693f7eed15579b14505cb40fb479b17c6
zarose <zarose0000ds@gmail.com>
2021-11-15 22:07:55 +0800

    feat: add front desk restaurants list

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,23 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ raw: true, nest: true, include: Category }).then(restaurants => {
+      restaurants = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50),
+        categoryName: r.Category.name
+      }))
+      console.log(restaurants)
+      return res.render('restaurants', { restaurants })
+    })
+  },
+  getRestaurant: (req, res) => {
+    Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
+    })
   }
 }
 module.exports = restController
--- routes/index.js
+++ routes/index.js
@@ -58,4 +58,5 @@ module.exports = (app, passport) => {
   
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
 }
