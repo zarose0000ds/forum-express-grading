308eb233f2000335ce035b97afe3c8f493760884
zarose <zarose0000ds@gmail.com>
2021-11-15 23:34:14 +0800

    feat: add paginator on restaurant list

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,24 +2,43 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const PAGE_LIMIT = 10
+
 const restController = {
   getRestaurants: (req, res) => {
     const where = {}
     let categoryId = ''
+    let offset = 0
 
+    // FOR CATEGORY FILTER
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
-      where.CategoryId = categoryId
+      where.categoryId = categoryId
     }
 
-    Restaurant.findAll({ raw: true, nest: true, include: Category, where }).then(restaurants => {
-      restaurants = restaurants.map(r => ({
+    // FOR PAGINATOR
+    if (req.query.page) {
+      offset = (req.query.page - 1) * PAGE_LIMIT
+    }
+
+    Restaurant.findAndCountAll({ raw: true, nest: true, include: Category, where, offset, limit: PAGE_LIMIT }).then(result => {
+      // PAGINATOR DATA
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / PAGE_LIMIT)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      console.log(page, pages, totalPage, prev, next)
+
+      // CLEAN UP RESTAURANT DATA
+      const restaurants = result.rows.map(r => ({
         ...r,
         description: r.description.substring(0, 50),
         categoryName: r.Category.name
       }))
       Category.findAll({ raw: true, nest: true }).then(categories => {
-        return res.render('restaurants', { restaurants, categories, categoryId })
+        return res.render('restaurants', { restaurants, categories, categoryId, page, totalPage, prev, next })
       })
     })
   },
